e10102f5954778e4c09bd6fa3d3ab953cd45365f
Calvin Montgomery <ctm0012@auburn.edu>
2015-02-22 20:52:04 -0600

    Add bounding and initial kernel timing support
    Since the Nelder-Mead algorithm does not take bounds into account,
    bounding is implemented by returning +inf for any point outside the
    allowable domain.
    
    Kernel timing is the timing data given by passing the -ta=nvidia,time
    flag to pgcc.  Currently, the time parsed by the algorithm is the total
    time reported at the top of the timing data, rather than the time of any
    specific kernel.  However, it still appears to be far more accurate and
    has less variation than OpenMP timers.

--- tuner.py
+++ tuner.py
@@ -6,15 +6,20 @@ from nelder_mead import *
 # Default compilation command
 PGCC_COMPILE = ('pgcc -acc -DNUM_GANGS={num_gangs} '
                 '-DVECTOR_LENGTH={vector_length} {source}')
+PGCC_COMPILE_KERNEL_TIMING = ('pgcc -acc -DNUM_GANGS={num_gangs} '
+                '-DVECTOR_LENGTH={vector_length} -ta=nvidia,time {source}')
 
 # Default regular expression matching the time output
 TIME_RE = r'(?:time)[=:\s]*([\d.]+)'
+KERNEL_TIMING_RE = re.compile(r'Accelerator Kernel Timing data\n'
+        r'(?:[^\n]*\n){2}'
+        r'\s*time\(us\): ([\d,]+)')
 
 def check_output(cmd):
     handle = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT, shell=True)
-    stdout, _ = handle.communicate()
-    return stdout.encode('utf8')
+            stderr=subprocess.PIPE, shell=True)
+    stdout, stderr = handle.communicate()
+    return stdout.encode('utf8'), (stderr.encode('utf8') if stderr else '')
 
 class TuningOptions(object):
     ''' Represents a set of options and constraints for tuning '''
@@ -29,11 +34,16 @@ class TuningOptions(object):
             vector_length_min=1,
             vector_length_max=1024,
             verbose=False,
-            method='Random Search'):
+            kernel_timing=False):
 
         self.source = source
         self.executable = executable
-        self.compile_command = (compile_command or PGCC_COMPILE)
+        if compile_command:
+            self.compile_command = compile_command
+        elif kernel_timing:
+            self.compile_command = PGCC_COMPILE_KERNEL_TIMING
+        else:
+            self.compile_command = PGCC_COMPILE
         self.repetitions = repetitions
         self.time_regexp = re.compile(time_regexp, re.I)
         self.num_gangs_min = num_gangs_min
@@ -41,7 +51,7 @@ class TuningOptions(object):
         self.vector_length_min = vector_length_min
         self.vector_length_max = vector_length_max
         self.verbose = verbose
-        self.method = method
+        self.kernel_timing = kernel_timing
 
 # From a set of tuning options, return a function that when called with a
 # num_gangs, vector_length, and optionally a number of repetitions (N), will
@@ -64,30 +74,43 @@ def gen_tuning_function(opts):
         for i in range(repetitions):
             if opts.verbose:
                 print('[{0}, {1}] {2}'.format(num_gangs, vector_length,
-                        opts.executable))
-            result = check_output(opts.executable)
+                        opts.executable), end=' ')
+            stdout, stderr = check_output(opts.executable)
 
-            match = opts.time_regexp.search(result)
-            if not match:
-                raise RuntimeError('Output from executable {0} did not contain '
-                        'any matches for the time regex {1}: {2}'.format(
-                        opts.executable, opts.time_regexp.pattern, result))
+            if opts.kernel_timing:
+                match = KERNEL_TIMING_RE.search(stderr)
+                if not match:
+                    raise RuntimeError('Output from executable {0} did not '
+                            'contain kernel timing data.'.format(
+                                opts.executable))
 
-            time = match.group(1)
+                time = float(match.group(1).replace(',', '')) * 1e-6
+            else:
+                match = opts.time_regexp.search(stdout)
+                if not match:
+                    raise RuntimeError('Output from executable {0} did not '
+                            'contain any matches for the time regex {1}: {2}'
+                            .format(opts.executable, opts.time_regexp.pattern,
+                            result))
+
+                time = match.group(1)
+            if opts.verbose:
+                print(time)
             results.append(float(time))
 
         return sum(results) / len(results)
     return fn
 
-def tune_scipy(opts):
+def tune(opts):
     run_test = gen_tuning_function(opts)
 
     def objective(x):
         x = map(int, x)
-        if opts.verbose:
-            print('OBJECTIVE', x)
 
-        if not 1 <= x[0] <= 1024 or not 1 <= x[1] <= 1024:
+        if x[0] < opts.num_gangs_min or x[0] > opts.num_gangs_max:
+            return float('+inf')
+
+        if x[1] < opts.vector_length_min or x[1] > opts.num_gangs_max:
             return float('+inf')
 
         return run_test(x[0], x[1], repetitions=opts.repetitions)
@@ -115,10 +138,11 @@ def main():
     parser.add_argument('-c', '--compile-command', type=str)
     parser.add_argument('-r', '--repetitions', type=int)
     parser.add_argument('-t', '--time-regexp', type=str)
-    #parser.add_argument('--num-gangs-min', type=int)
-    #parser.add_argument('--num-gangs-max', type=int)
-    #parser.add_argument('--vector-length-min', type=int)
-    #parser.add_argument('--vector-length-max', type=int)
+    parser.add_argument('-k', '--kernel-timing', action='store_true')
+    parser.add_argument('--num-gangs-min', type=int)
+    parser.add_argument('--num-gangs-max', type=int)
+    parser.add_argument('--vector-length-min', type=int)
+    parser.add_argument('--vector-length-max', type=int)
     parser.add_argument('-v', '--verbose', action='store_true')
 
     args = parser.parse_args()
@@ -133,7 +157,7 @@ def main():
     if args.verbose:
         print('TuningOptions: {0}'.format(t.__dict__))
 
-    tune_scipy(t)
+    tune(t)
 
 
 if __name__ == '__main__':
